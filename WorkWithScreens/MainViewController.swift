8a54b63ed6213378770dd344dc30c79851bfcb39
Egor Syrtcov <sev_84@inbox.ru>
2020-02-05 11:49:44 +0300

    Create new project

---
+++ WorkWithScreens/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WorkWithScreens
+//
+//  Created by Egor Syrtcov on 2/5/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
