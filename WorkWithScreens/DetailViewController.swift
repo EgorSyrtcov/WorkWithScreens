6938fe6b0e8be493b8736dfc1db32b56b1b3c20a
Egor Syrtcov <sev_84@inbox.ru>
2020-02-05 12:20:01 +0300

    Создание второго ViewController(DetailViewController) куда будем переходить

---
+++ WorkWithScreens/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  WorkWithScreens
+//
+//  Created by Egor Syrtcov on 2/5/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, StoryboardInitializable {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
