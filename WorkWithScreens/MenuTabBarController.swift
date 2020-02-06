aa2feea100bfb00952d28ba866604daae95b659d
Egor Syrtcov <sev_84@inbox.ru>
2020-02-06 11:59:06 +0300

    небольшие изменения в tabBar

---
+++ WorkWithScreens/MenuTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MenuTabBarController.swift
+//  WorkWithScreens
+//
+//  Created by Egor Syrtcov on 2/6/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import UIKit
+
+class MenuTabBarController: UITabBarController, StoryboardInitializable {
+    
+    private let newVC = NewsViewController()
+    private let emailVC = EmailViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [createController(title: "NewVC", imageName: "news", vc: newVC), createController(title: "EmailVC", imageName: "mail", vc: emailVC)]
+        
+    }
+    
+    private func createController(title: String, imageName:String, vc: UIViewController) -> UINavigationController {
+        let recentVc = UINavigationController(rootViewController: vc)
+        recentVc.tabBarItem.title = title
+        recentVc.tabBarItem.image = UIImage(named: imageName)
+        return recentVc
+    }
+}
