6f87de7aa992810d59e1682b912ccbb22d9891e4
Egor Syrtcov <sev_84@inbox.ru>
2020-02-25 11:06:03 +0300

    Update AppDelegate print

--- WorkWithScreens/AppDelegate.swift
+++ WorkWithScreens/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         print("Fix bug done")
+        print("Hello Swift")
         return true
     }
 
