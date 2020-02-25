56ffd0926839f2f862378cc0aad33d458475cac9
Egor Syrtcov <sev_84@inbox.ru>
2020-02-25 11:33:01 +0300

    master branch commit

--- WorkWithScreens/AppDelegate.swift
+++ WorkWithScreens/AppDelegate.swift
@@ -16,8 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print("Fix bug done")
-        print("Hello Swift")
+        print("Master branch print")
         return true
     }
 
